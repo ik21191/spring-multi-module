3f51f49753203d46069193cd06a651a2a56df7a3
Imran Khan <imrankhan.in3@gmail.com>
2025-04-09 16:58:50 +0530

    [spring-boot-rest:v1]:Adding ADMIN user in application.properties and created out-of-memory scenario

--- spring-boot-rest/README.md
+++ spring-boot-rest/README.md
@@ -2,3 +2,12 @@
 
 - This application contains REST endpoints to showcase the spring boot rest implementation.
 - It is using `spring-boot-data-jpa` dependency for database interaction.
+
+**Create OutOfMemory issue**
+- You can execute the application using below command and try to call this [localhost](http://localhost:8888/out-of-memory)
+
+```
+java -Xms256m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=d://heap_dump.hprof -jar .spring-boot-rest-2.5.14.jar
+```
+
+Then you can analyze the dump using **Memory Analyzer Tool(MAT)**
--- spring-boot-rest/src/main/java/com/mypack/spring/rest/controller/HelloController.java
+++ spring-boot-rest/src/main/java/com/mypack/spring/rest/controller/HelloController.java
@@ -2,9 +2,9 @@ package com.mypack.spring.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -17,5 +17,20 @@ public class HelloController {
       throw new Exception();
     }
     return "Hello, the time at the server is now " + new Date() + "\n";
+  }
+  
+  @GetMapping("/out-of-memory")
+  public String createOutOfMemoryError() throws Exception {
+    System.out.println("Creating outOfMemory error............");
+    List<byte[]> list = new ArrayList<>();
+    int index = 1;
+    while (true) {
+      // Allocate 1MB each loop (1 x 1024 x 1024 = 1048576 bytes)
+      byte[] b = new byte[1048576];
+      list.add(b);
+      // Get the current free memory
+      Runtime rt = Runtime.getRuntime();
+      System.out.printf("[%d] free memory: %s%n", index++, rt.freeMemory());
+    }
   }
 }
